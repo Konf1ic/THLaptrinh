23e710e694dfbdf2ad7b91bde5dc25e994bb3e3b
Konf1ic <kenbui2809@gmail.com>
2022-11-25 09:17:32 +0700

    up bai tap

---
+++ Tính chu vi diện tích các hình tròn/main.js
@@ -0,0 +1,12 @@
+var a = null;
+var pi = 3.14;
+var c = null;
+let R= prompt("Nhập bán kính hình tròn")
+function M() {
+    let c= 2*pi*R;
+    document.write('chu vi: ' +C);
+}
+function show() {
+    let S= pi*R^2;
+    document.write('Dien tich: ' +S);
+}
